5c511a41c0335118febc5f33ee81ba3b39b5e05e
MCannon33 <marialcannon@gmail.com>
2020-11-10 22:03:38 -0500

    add p back

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,6 +118,14 @@ var index = $(this)
 .closest(".list-group-item")
 .index();
 
+// recreate p element
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+// replace textarea with p element
+$(this).replaceWith(taskP);
+
 tasks[status][index].text = text;
 saveTasks();
 });
